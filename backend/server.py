f1f7ccccae802079c413c94c2bec056e28a75d01
rifflere <82406585+rifflere@users.noreply.github.com>
2026-01-24 18:20:19 -0800

    Added Get and Post routes for photo and note

--- backend/server.py
+++ backend/server.py
@@ -1,18 +1,29 @@
 from fastapi import FastAPI
 import uvicorn
 from sqlmodel import select, delete
-
-from db import Entry, create_db_and_tables, get_session
+from pydantic import BaseModel
+from db import Photo, Note, Entry, create_db_and_tables, get_session
 
 app = FastAPI()
 
 # Photo - Name and Date
 # Sticky Note - Date and Text
 
+# Classes for Photo and Note
+class PhotoCreate(BaseModel):
+    name: str
+    date: str | None = None
+
+
+class NoteCreate(BaseModel):
+    text: str
+    date: str | None = None
+
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
 
+## GET routes
 @app.get("/")
 def main_page():
     return {"page": "main"}
@@ -38,6 +49,39 @@ def add_page(text: str = "default entry"):
         session.commit()
         session.refresh(entry)
         return {"page": "add", "added": entry}
+    
+@app.get("/photos")
+def get_all_photos():
+    with get_session() as session:
+        photos = session.exec(select(Photo)).all()
+        return {"photos": photos}
+
+
+@app.get("/notes")
+def get_all_notes():
+    with get_session() as session:
+        notes = session.exec(select(Note)).all()
+        return {"notes": notes}
+
+## Post Routes
+@app.post("/photos")
+def create_photo(photo: PhotoCreate):
+    with get_session() as session:
+        db_photo = Photo(name=photo.name, date=photo.date)
+        session.add(db_photo)
+        session.commit()
+        session.refresh(db_photo)
+        return {"status": "added", "photo": db_photo}
+
+
+@app.post("/notes")
+def create_note(note: NoteCreate):
+    with get_session() as session:
+        db_note = Note(text=note.text, date=note.date)
+        session.add(db_note)
+        session.commit()
+        session.refresh(db_note)
+        return {"status": "added", "note": db_note}
 
 def start_web_server():
     uvicorn.run(app, host="127.0.0.1", port=3000, log_level="info")
